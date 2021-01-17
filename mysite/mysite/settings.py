e49c439d9510f6246e0a59f0e6d6874e80642a1e
Osama Imran <osama.imran@innovocrete.com>
2021-01-17 15:33:01 +0500

    settings changed

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '87$#^4jb#u+5y+&b+y$fghnfkav%w%8+-1v8f-kbzul2igvf58'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
